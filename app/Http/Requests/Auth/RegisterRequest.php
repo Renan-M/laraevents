a0333af828532e8b0e214c38fea2e4ee5b0d2a6d
renan-arch <renanonho30@gmail.com>
2021-11-17 21:23:22 -0300

    projeto iniciado

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    public function rules()
+    {
+        return [
+            'user.name' => 'required',
+            'user.email' => ['required', 'email'],
+            'user.cpf' => 'required',
+            'user.password' => ['required', 'min:8'],
+            'phones.0.number' => ['required', 'size:14'],
+            'phones.1.number' => ['required', 'size:15'],
+            'address.cep' => 'required',
+            'address.uf' => ['required', 'size:2'],
+            'address.city' => 'required',
+            'address.street' => 'required',
+            'address.number' => ['required', 'numeric', 'integer'],
+            'address.district' => 'required',
+            'address.complement' => ['nullable', 'max:25'],
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'user.name' => 'nome',
+            'user.email' => 'email',
+            'user.cpf' => 'CPF',
+            'user.password' => 'senha',
+            'phones.0.number' => 'telefone',
+            'phones.1.number' => 'celular',
+            'address.cep' => 'CEP',
+            'address.street' => 'logradouro',
+            'address.number' => 'número',
+            'address.uf' => 'UF',
+            'addres.city' => 'cidade',
+            'address.district' => 'bairro',
+            'address.complement' => 'complemento',
+        ];
+    }
+}
